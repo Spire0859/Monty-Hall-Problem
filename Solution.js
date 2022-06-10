12c21e3e7b2339df9c9cc14eb42aec6426534123
Tanim K <tanim0859@gmail.com>
2022-06-10 15:40:15 -0400

    all done

--- Solution.js
+++ Solution.js
@@ -29,29 +29,23 @@ class Statistics {
         this.gamesWithDoorChangeLost = [];
     }
 
+    // gives the win rate of each type of gameplay
     percentatge() {
         
-       console.log (this.gamesWithSameDoorWon.length /  (this.gamesWithSameDoorWon.length + this.gamesWithSameDoorLost.length) * 100 ,"% of games were won when not switching door.")
+       console.log (Math.trunc(this.gamesWithSameDoorWon.length /  (this.gamesWithSameDoorWon.length + this.gamesWithSameDoorLost.length) * 100) ,"% of games were won when not switching door.")
 
 
 
     }
 
     percentatgeS() {
-        console.log (this.gamesWithDoorChangeWon.length /  (this.gamesWithDoorChangeWon.length + this.gamesWithDoorChangeLost.length) * 100 , "% of games were won when switching door.")
+        console.log (Math.trunc(this.gamesWithDoorChangeWon.length /  (this.gamesWithDoorChangeWon.length + this.gamesWithDoorChangeLost.length) * 100) , "% of games were won when switching door.")
 
     }
 
 }
 
-function shuffleArray(array) {
-    for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-    }
-}
+
 
 function playGameNoSwitch(array,numOfGameObj,arrayStat){
 
@@ -60,6 +54,8 @@ let guess;
 let reset; 
 
 for(let i = 0; i < numOfGameObj; i++){
+
+// resets all doors to closed and picks a random door to have a car
 
 guess = Math.floor(Math.random() * 3)
 reset = Math.floor(Math.random() * 3)
@@ -72,6 +68,7 @@ array[i].doors[reset].isCar = true;
     array[i].doorPicked = guess;
     array[i].finalPick = guess;
    
+// very simple checks if door picked has a car if yes send that game to the statiscs array
 
 if( array[i].doors[array[i].finalPick].isCar ){
     arrayStat.gamesWithSameDoorWon.push(array[i]);
@@ -82,6 +79,8 @@ if( array[i].doors[array[i].finalPick].isCar ){
     array[i].won = false;
 
 }else{
+    //this is here just in case things mess up
+
     console.log("woops");
 }
 
@@ -95,6 +94,8 @@ if( array[i].doors[array[i].finalPick].isCar ){
 
 
 function playGameWithSwitch(array1,numOfGameObj1,arrayStat1){
+
+    //same thing here as the funcction above, we reset all doors and set a random door to have a car
 
     let guess; 
     let reset;
@@ -117,10 +118,8 @@ function playGameWithSwitch(array1,numOfGameObj1,arrayStat1){
 
     array1[i].doors[array1[i].doorPicked].opened = true;
 
-    
-    
 
-
+        //the code bellow, we look for a door that does not have a car and is not one we picked so essentilally we look for the other goat door to reveal it
     
        if ( !array1[i].doors[0].opened  && !array1[i].doors[0].isCar){
         array1[i].doors[0].opened = true;
@@ -134,6 +133,8 @@ function playGameWithSwitch(array1,numOfGameObj1,arrayStat1){
         array1[i].doors[2].opened = true;
 
     }
+
+    //after opening goat door, we set final pick to the door the game will pick
 
     if (!array1[i].doors[0].opened){
 
@@ -150,8 +151,11 @@ function playGameWithSwitch(array1,numOfGameObj1,arrayStat1){
     }
    
     if ( array1[i].finalPick == array1[i].doorPicked){
+        //also here to see if anything messes up
         console.log("yesh")
     }
+
+    //sends which game won into the array of statistics
 
     if( array1[i].doors[array1[i].finalPick].isCar ){
         
@@ -178,14 +182,16 @@ function playGameWithSwitch(array1,numOfGameObj1,arrayStat1){
  let gamesList = new Array();
  let gamesList1 = new Array();
 
- let numOfGame =  1001;  // change this to user input
+ let numOfGame =  1500;  // change this to how many games you wish to run
+
  let numOfDoor = new Array();
  let numOfDoor1 = new Array();
 
  let statistics = new Statistics();
  let statistics1 = new Statistics();
 
-
+// the reason there is two two sets of numOfDoor is because instead of A. making alot of doors for  each game, we cann just reuse the same 3 doors by resetting them which i do in the fucntions above. B. its so if ever we wanna call say .isCar of a type of game for which ever instance of game,
+// we can pull it both for the game where we swtich door and dont switch door if we wnated to 
  numOfDoor.push(new Door(0, false));
  numOfDoor.push(new Door(1, false));
  numOfDoor.push(new Door(2, false));
@@ -194,6 +200,8 @@ function playGameWithSwitch(array1,numOfGameObj1,arrayStat1){
  numOfDoor1.push(new Door(1, false));
  numOfDoor1.push(new Door(2, false));
 
+
+ //makes the number of games wanted
 for (let i = 0; i < numOfGame; i++){
 
     gamesList.push(new Game());
@@ -204,6 +212,8 @@ for (let i = 0; i < numOfGame; i++){
 
 }
 
+//puts doors in each game
+
 for(let i = 0; i < numOfGame; i ++){
 
     for(let y = 0;y < 3 ; y++ ){
@@ -213,12 +223,10 @@ for(let i = 0; i < numOfGame; i ++){
 
        
     }
-    //shuffleArray(gamesList[i].doors); 
-    //shuffleArray(gamesList1[i].doors); 
-
+    
 }
 
-//console.log(gamesList[48].doors[1]);
+//runs the game then deos the calculation
 
 playGameNoSwitch(gamesList,numOfGame,statistics);
 
